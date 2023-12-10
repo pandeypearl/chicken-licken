592586dfae269ed60ab975bb72c052e2ead77116
Pretty Pandey <prettypandeypearl@gmail.com>
2023-12-10 19:17:36 +0200

    Top Navbar

--- client/src/components/Layout/index.js
+++ client/src/components/Layout/index.js
@@ -1,9 +1,11 @@
 import { Outlet } from 'react-router-dom';
+import TopNav from '../TopNav';
 import './index.scss';
 
 const Layout = () => {
     return (
         <div className='App'>
+            <TopNav />
             <div className='page'>
                 <Outlet />
             </div>
---
+++ client/src/components/TopNav/index.js
@@ -0,0 +1,32 @@
+import { Link,NavLink } from 'react-router-dom';
+import Logo from '../../assets/logo.svg'; 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {faBagShopping} from '@fortawesome/free-solid-svg-icons';
+import './index.scss';
+
+const TopNav = () => {
+
+    return (
+        <nav className='top-nav'>
+            <div>
+                <Link className='logo' to='/'>
+                    <img src={Logo} alt='Logo' />
+                </Link>
+            </div>
+
+            <div>
+                <NavLink exact='true' activeClassName='active' to='/'>menu</NavLink>
+                <NavLink exact='true' activeClassName='active' to='/'>store finder</NavLink>
+                <NavLink exact='true' activeClassName='active' to='/'>delivery</NavLink>
+                <NavLink exact='true' activeClassName='active' to='/'>search</NavLink>
+            </div>
+
+            <div>
+                <NavLink exact='true' activeClassName='active' to='/'><FontAwesomeIcon icon={faBagShopping} color='#FF8C00' /></NavLink>
+            </div>
+        </nav>
+    )
+
+}
+
+export default TopNav;
