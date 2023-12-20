a3ab898c7f994bbed644c2736658fe955e208e87
Pretty Pandey <prettypandeypearl@gmail.com>
2023-12-20 20:50:23 +0200

    Adding data file and uploading project images

---
+++ client/src/data/chickenlicken-data.js
@@ -0,0 +1,145 @@
+let chickenLickenData = [
+    {
+        id: "munu-item-001",
+        section: "Just Hotwings",
+        name: "Hotwings 6",
+        price: "38.00",
+        desc: "6 Hotwings",
+        image:"img/original_images/hotwings/Hotwings_6.jpg",
+    },
+    {
+        id: "munu-item-002",
+        section: "Just Hotwings",
+        name: "Hotwings 12",
+        price: "73.00",
+        desc: "12 Hotwings",
+        image:"img/original_images/hotwings/Hotwings_12.jpg",
+
+    },
+    {
+        id: "munu-item-003",
+        section: "Just Hotwings",
+        name: "Hotwings 24",
+        price: "135.00",
+        desc: "24 Hotwings",
+        image:"img/original_images/hotwings/Hotwings_24.jpg",
+
+    },
+    {
+        id: "munu-item-004",
+        section: "Just Hotwings",
+        name: "Hotwings 36",
+        price: "73.00",
+        desc: "36 Hotwings",
+        image:"img/original_images/hotwings/Hotwings_36.jpg",
+
+    },
+    {
+        id: "munu-item-005",
+        section: "Soulicious Specials",
+        name: "Hotwings Soul 6",
+        price: "42.00",
+        desc: "6 Hotwings® + a portion of Soul Fries™",
+        image:"img/original_images/soulicious_specials/final/HotwingsSoul6.png",
+
+    },
+    {
+        id: "munu-item-006",
+        section: "Soulicious Specials",
+        name: "Rock My Soul 3 Straight",
+        price: "48.00",
+        desc: "3 pieces SoulFire® chicken + 1 sachet SoulFire® sauce \n *No substitutions*",
+        image:"img/original_images/soulicious_specials/RockMySoul3Straight.jpg",
+
+    },
+    {
+        id: "munu-item-007",
+        section: "Soulicious Specials",
+        name: "Rock My Soul 3",
+        price: "51.00",
+        desc: "3 pieces SoulFire® chicken + a portion of Soul Fries™ \n + 1 sachet SoulFire® sauce \n *No substitutions*",
+        image:"img/original_images/soulicious_specials/RockMySoul3.jpg",
+
+    },
+    {
+        id: "munu-item-008",
+        section: "Soulicious Specials",
+        name: "Hotwings 6 Deluxe",
+        price: "58.00",
+        desc: "6 Hotwings® + a portion of Soul Fries™ + 300ml Coke",
+        image:"img/original_images/soulicious_specials/300ml-hotwings-6-deluxe.png",
+
+    },
+    {
+        id: "munu-item-035",
+        section: "Easy Bucks Menu",
+        name: "Slider Snack 2",
+        price: "28.00",
+        desc: "2 Original Slyders® + a portion of Soul Fries™",
+        image:"img/original_images/easy_bucks/SliderSnack2.jpg",
+
+    },
+    {
+        id: "munu-item-036",
+        section: "Easy Bucks Menu",
+        name: "Popcorn Chicken Shaker",
+        price: "28.00",
+        desc: "Popcorn Chicken® in a shaker cup + 2 sachets SoulFire® sauce or 2 sachets of Uncle Jessup's® Ketchup to shake up the taste!",
+        image:"img/original_images/easy_bucks/PopcornShaker.jpg",
+
+    },
+    {
+        id: "munu-item-037",
+        section: "Easy Bucks Menu",
+        name: "Hotwings Snack",
+        price: "28.00",
+        desc: "4 Hotwings® + a portion of Soul Fries™",
+        image:"img/original_images/easy_bucks/Hotwings_Snack.jpg",
+
+    },
+    {
+        id: "munu-item-038",
+        section: "Easy Bucks Menu",
+        name: "Soul Mix Snack",
+        price: "28.00",
+        desc: "2 Hotwings® + a Slider® + a portion of Soul Fries™",
+        image:"img/original_images/easy_bucks/SoulmixSnack.jpg",
+
+    },
+    {
+        id: "munu-item-040",
+        section: "Just Chick'n Burgers",
+        name: "Double Chicken Boerie Burger",
+        price: "28.00",
+        desc: "2 Chick’n Boerie™ Burger patties + lettuce + 3 pickles + Licken® sauce on a toasted sesame bun",
+        image:"img/original_images/chicken_burgers/ChickenBoerieBurger.jpg",
+
+    },
+    {
+        id: "munu-item-041",
+        section: "Just Chick'n Burgers",
+        name: "Love Me Tender Burger",
+        price: "34.00",
+        desc: "Chicken fillet + lettuce + tomato + Dreamy Creamy® sauce on a toasted sesame bun",
+        image:"img/original_images/chicken_burgers/LoveMeTender.jpg",
+
+    },
+    {
+        id: "munu-item-042",
+        section: "Just Chick'n Burgers",
+        name: "Love Me Hot Burger",
+        price: "34.00",
+        desc: "Chicken fillet + lettuce + tomato + Soulfirenaise™ sauce on a toasted sesame bun",
+        image:"img/original_images/chicken_burgers/LoveMeHot.jpg",
+
+    },
+    {
+        id: "munu-item-043",
+        section: "Just Chick'n Burgers",
+        name: "Love Me Tender Cheese Burger",
+        price: "36.00",
+        desc: "Chicken fillet + lettuce + tomato + Dreamy Creamy® sauce + a slice of cheese on a toasted sesame bun",
+        image:"img/original_images/chicken_burgers/LoveMeTender_WithCheese.jpg",
+
+    },
+]
