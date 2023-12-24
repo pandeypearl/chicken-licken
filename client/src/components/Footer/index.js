bbb3ec06ca3795ebacd6c0b5fd970b5951206b41
Pretty Pandey <prettypandeypearl@gmail.com>
2023-12-24 19:47:27 +0200

    Footer component positioning and social link paths

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -6,11 +6,23 @@ import './index.scss';
 const Footer = () => {
     return (
         <footer className='footer'>
-            <div>
-                <Link><FontAwesomeIcon icon={faFacebook} color='#FF8C00' /></Link>
-                <Link><FontAwesomeIcon icon={faTwitter} color='#FF8C00' /></Link>
-                <Link><FontAwesomeIcon icon={faYoutube} color='#FF8C00' /></Link>
-            </div>
+            <ul>
+                <li>
+                    <a target='_blank' rel='noreferrer' href='https://www.facebook.com/ChickenLickenSA/'>
+                        <FontAwesomeIcon icon={faFacebook} color='#FF8C00' />
+                    </a>
+                </li>
+                <li>
+                    <a target='_blank' rel='noreferrer' href='https://twitter.com/ChickenLickenSA'>
+                        <FontAwesomeIcon icon={faTwitter} color='#FF8C00' />
+                    </a>
+                </li>
+                <li>
+                    <a target='_blank' rel='noreferrer' href='https://www.youtube.com/user/ChickenLickenSA'>
+                        <FontAwesomeIcon icon={faYoutube} color='#FF8C00' />
+                    </a>
+                </li>
+            </ul>
             <div>
                 <Link>franchising</Link>
                 <Link>talk to us</Link>
