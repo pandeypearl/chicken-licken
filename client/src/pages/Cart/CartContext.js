9272a8f2cfa72385fe87e761dbfd7cdbb3d249fa
Pretty Pandey <prettypandeypearl@gmail.com>
2024-01-15 20:26:06 +0200

    Adding localStorage functionality to CartContextProvider

--- client/src/pages/Cart/CartContext.js
+++ client/src/pages/Cart/CartContext.js
@@ -16,6 +16,26 @@ const getDefaultCart = () => {
 export const CartContextProvider = (props) => {
     const [cartItems, setCartItems] = useState(getDefaultCart());
 
+    useEffect(() => {
+        // Retrieve cart items from localStorage on component mount
+        const storedCartItems = localStorage.getItem('cartItems');
+        if (storedCartItems) {
+            try {
+                setCartItems(JSON.parse(storedCartItems) || getDefaultCart());
+            } catch (error) {
+                console.error('Error parsing stored cart items:', error);
+                setCartItems(getDefaultCart());
+            }
+        } else {
+            setCartItems(getDefaultCart());
+        }
+    }, []);
+
+    // Use useEffect to save cart items to localStorage whenever it changes
+    useEffect(() => {
+        localStorage.setItem('cartItems', JSON.stringify(cartItems));
+    }, [cartItems]);
+
     const getTotalCartAmount = () => {
         let totalAmount = 0;
         for (const item in cartItems) {
@@ -45,9 +65,9 @@ export const CartContextProvider = (props) => {
         setCartItems(getDefaultCart());
     }
 
-    useEffect(() => {
-        console.log('Cart Items after addToCart:', cartItems);
-    }, [cartItems]);
+    // useEffect(() => {
+    //     console.log('Cart Items after addToCart:', cartItems);
+    // }, [cartItems]);
 
     const contextValue = {
         cartItems,
